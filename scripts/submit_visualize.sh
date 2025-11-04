2fb71c36271abab828e874fbbaa859862da7a1ce
Ziling2023 <ziling2023@gmail.com>
2025-11-04 07:37:28 +0000

    visualize

--- scripts/submit_visualize.sh
+++ scripts/submit_visualize.sh
@@ -1,8 +1,27 @@
+set -e
+
+CLUSTER_NAME="my-cluster"
+NUM_WORKERS=2
+MASTER_DISK=100
+WORKER_DISK=100
+
+
+gcloud dataproc clusters create $CLUSTER_NAME \
+  --region=asia-southeast1 \
+  --num-workers=$NUM_WORKERS \
+  --worker-machine-type=n2-standard-4 \
+  --master-machine-type=n2-standard-4 \
+  --master-boot-disk-size=$MASTER_DISK \
+  --worker-boot-disk-size=$WORKER_DISK \
+  --image-version="2.2-debian12" \
+  --optional-components=JUPYTER \
+  --enable-component-gateway \
+
 gcloud dataproc jobs submit pyspark \
     test/visualize.py \
     --cluster=mycluster \
     --region=asia-southeast1 \
-    --py-files=src/time_cv.py,src/utils.py \
+    --py-files="src/" \
     -- \
     --train-path="gs://spark-result/train_withds1/" \
     --test-path="gs://spark-result/test_withds1/" \
--- test/data_split.py
+++ test/data_split.py
@@ -15,11 +15,21 @@ output_bucket = "gs://spark-result"
 
 spark = SparkSession.builder.appName("WeatherProcessing").getOrCreate()
 
-print("Step 0: Reading files from", input_path)
+print("Step: Reading files from", input_path)
 df_filtered = spark.read.parquet(input_path)
 
+
 # ---------
-# Step 6: Train/test split
+# Step 6: Filter TMP
+# ---------
+lower, upper = df_filtered.approxQuantile("TMP", [0.05, 0.95], 0.01)
+print(f"Step 6: Lower threshold: {lower}, Upper threshold: {upper}")
+
+df_filtered = df_filtered.filter((df_filtered["TMP"] >= lower) & (df_filtered["TMP"] <= upper))
+
+
+# ---------
+# Step 7: Train/test split
 # ---------
 min_date, max_date = df_filtered.agg(
     F.min("DATE_TS").alias("min"),
@@ -30,9 +40,9 @@ cutoff = min_date + (max_date - min_date) * 0.7
 
 train_df = df_filtered.filter(F.col("DATE_TS") <= cutoff)
 test_df  = df_filtered.filter(F.col("DATE_TS") > cutoff)
-print("Step 8: Train count:", train_df.count(), "Test count:", test_df.count())
+print("Step 7: Train count:", train_df.count(), "Test count:", test_df.count())
 
-train_df.write.mode("overwrite").parquet(os.path.join(output_bucket, "train_withds"))
-test_df.write.mode("overwrite").parquet(os.path.join(output_bucket, "test_withds"))
+train_df.write.mode("overwrite").parquet(os.path.join(output_bucket, "train_withds1"))
+test_df.write.mode("overwrite").parquet(os.path.join(output_bucket, "test_withds1"))
 print("All steps completed. CSVs saved to", output_bucket)
 
